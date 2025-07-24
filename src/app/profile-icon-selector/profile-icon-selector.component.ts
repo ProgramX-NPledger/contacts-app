2885f43fcc63893a76113e69905ef927dc0fdb1e
ProgramX-NPledger <nathan@programx.co.uk>
2025-07-24 19:32:36 +0100

    End 5/6

---
+++ src/app/profile-icon-selector/profile-icon-selector.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { profileIconNames } from './profile-icon-names';
+
+@Component({
+  selector: 'con-profile-icon-selector',
+  templateUrl: './profile-icon-selector.component.html',
+  styleUrls: ['./profile-icon-selector.component.css']
+})
+export class ProfileIconSelectorComponent {
+  profileIcons = profileIconNames;
+  showAllIcons: boolean = true;
+  selectedIcon: string | null = null;
+
+  iconSelected(icon: string) {
+    this.selectedIcon = icon;
+    this.showAllIcons = false;
+    // Emit the selected icon if needed, e.g., using an EventEmitter
+    console.log(`Selected icon: ${icon}`);
+  }
+
+}
